84d1e476f1704c9612321dd70196e611564b6870
tippitytapp <marc.tapp@gmail.com>
2020-05-06 17:15:22 -0400

    got the add a comment with both a post and user validation

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// code away!
+// Custom MiddleWare
+const CMW = require('./customMiddleWare.js')
+
+// dependency imports
+const express = require('express');
+const colors= require('colors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+// define server and port
+const server = express();
+const PORT = 4401;
+// 3rd Party MiddleWare
+server.use(express.json());
+server.use(CMW.logger);
+server.use(helmet());
+// router imports
+const PostsRouter = require('./posts/postRouter.js');
+const UsersRouter = require('./users/userRouter.js');
+
+
+server.get('/', (req, res) => {
+  res.status(200).json({
+      status: "200 OK",
+      message: "Welcome to Node API 3 - MiddleWare Day"
+  });
+});
+
+server.use('/api/posts', PostsRouter)
+server.use('/api/users', UsersRouter)
+
+// server listen
+server.listen(PORT, ()=>{
+    console.log(`\nServer Listening on http://localhost:${PORT}\n`.bgCyan.black.bold)
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,17 +1,48 @@
 const express = require('express');
-
+const Users = require('./userDb.js')
+const Posts = require('../posts/postDb.js')
+const CMW = require('../customMiddleWare.js')
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // do your magic!
+  Users.insert(req.body)
+  .then(user => {
+    res.status(201).json({
+      user:user
+    })
+  .catch(error=> {
+      res.status(500).json({
+        errorMessage: "Server Error",
+        error: error
+      })
+    })
+  })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // do your magic!
+  Posts.insert(req.body)
+  .then(post=>{
+    res.status(201).json(post)
+  })
+  .catch(error => {
+    res.status(500).json({
+      errorMessage: "internal error",
+      error: error
+    })
+  })
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  Users.get(req.params)
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(error => {
+    res.status(500).json(error)
+  })
 });
 
 router.get('/:id', (req, res) => {
@@ -33,15 +64,52 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const id = req.params.id;
+  Users.getById(id)
+  .then(user=>{
+    if(!user){
+      res.status(404).json({
+        errorMessage: "No User Matching that ID found"
+      })
+    }else{
+      req.user = req.params.id
+      next();
+    }
+  })
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  if(!req.body){
+    res.status(400).json({
+      errorMessage: "Missing user information"
+    })
+  }else if(!req.body.name){
+    res.status(400).json({
+      errorMessage: "Missing user name"
+    })
+  }else{
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  if(!req.body){
+    res.status(400).json({
+      errorMessage: "Missing post information"
+    })
+  }else if(!req.body.text){
+    res.status(400).json({
+      errorMessage: "Missing post text"
+    })
+  }else if(!req.body.user_id){
+    res.status(400).json({
+      errorMessage: "Missing user_id"
+    })
+  }else{
+  next();
+}
 }
 
 module.exports = router;
